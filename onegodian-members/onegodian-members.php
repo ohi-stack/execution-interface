3dabc10b54482798800c053045d1881c6a9fdaef
ohi-stack <onegodianone@gmail.com>
2026-09-16 21:57:17 -0400

    Load OneGodian Members legacy compatibility layer

--- onegodian-members/onegodian-members.php
+++ onegodian-members/onegodian-members.php
@@ -18,10 +18,12 @@ define('OGM_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 require_once OGM_PLUGIN_DIR . 'includes/class-onegodian-members-contributors-affiliates.php';
 require_once OGM_PLUGIN_DIR . 'includes/class-onegodian-members-production-experience.php';
+require_once OGM_PLUGIN_DIR . 'includes/class-onegodian-members-compatibility.php';
 
 register_activation_hook(__FILE__, array('OneGodian_Members_Production_Experience', 'activate'));
 
 add_action('plugins_loaded', static function () {
     OneGodian_Members_Contributors_Affiliates::instance();
     OneGodian_Members_Production_Experience::instance();
+    OneGodian_Members_Compatibility::instance();
 });
